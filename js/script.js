67892065c18d16837f042b59f77a5202820b4e76
Celso Fuentes <celsofuentes957@gmail.com>
2023-12-31 12:03:49 -0500

    added all original files from the project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+$(document).ready(() => {
+    console.clear();
+
+    $('#chat-icon').click(() => {
+        console.log('clicked');
+        $('#chat-box').toggle(400);
+    });
+
+});
